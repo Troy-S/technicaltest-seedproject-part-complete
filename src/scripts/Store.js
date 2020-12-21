06ab1f5eccd1cff265f4a9b7174151b2e78caf07
Troy-S <troysheed@gmail.com>
2020-12-21 13:41:16 +0000

    Added JsDoc for filter() & get deals() - Returning categories via spread operator - Ready for pseudo code - #time 15m

--- src/scripts/Store.js
+++ src/scripts/Store.js
@@ -10,12 +10,38 @@ class Store extends Observable {
     };
   }
 
+  /**
+   * Binding filter() to deals getter
+   *
+   * @author Troy Sheed
+   * @return {[Object]} Allowing access to a dynamically computed property value
+   */
   get deals() {
+    // Returning dynamically computed filter()
     return this.filter();
   }
 
+  /**
+   * Filters a new object array of categories from deals in our store
+   *
+   * @author Troy Sheed
+   * @type {Object} - An object array containing our default deals
+   * @param {Object[]} categories
+   * @return {Object} - Returns a list of categories which match our criteria
+   */
   filter() {
-    return this.state.deals;
+  /**
+   * Instead of returning our deals directly we want to use the spread param
+   *
+   * According to MDN: Spread syntax (...) allows an iterable such as an array
+   * expression or string to be expanded in places where zero or more arguments
+   * (for function calls) or elements (for array literals) are expected, or an
+   * object expression to be expanded in places where zero or more key-value
+   * pairs (for object literals) are expected.
+   */
+    // return this.state.deals;
+    let categories = [...this.state.deals];
+    return categories;
   }
 
   setDeals(data) {
