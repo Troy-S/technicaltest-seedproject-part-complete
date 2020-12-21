bd051fc28ece2b0b65944d4690885a8a76d1f7d9
Troy-S <troysheed@gmail.com>
2020-12-21 13:01:47 +0000

    Added basic structure for our unit tests and pseudo code #time 10m

--- src/scripts/__tests__/Store.js
+++ src/scripts/__tests__/Store.js
@@ -12,6 +12,7 @@ import mockData from "../../../public/db.json";
  */
 
 describe("filter", () => {
+
   it("should return all deals when no filters applied", () => {
     // Arrange
     const storeUnitTest = new Store();
@@ -24,30 +25,48 @@ describe("filter", () => {
 
   it("should return broadband deals when filtered by broadband", () => {
     // Arrange
-    // Act
-    // Assert
+    const storeUnitTest = new Store();
+    storeUnitTest.setDeals(mockData.deals);
+    // Act - Use Store.js setProductFilter to match criteria filtered by?
+    // Would be lowercase 'broadband' as we will normalize the results
+    storeUnitTest.setProductFilter('broadband');
+    /**
+     *  Assert - use expect assertion to compare results - Using toBe as it's
+     *  a primative type
+     */
+    expect(storeUnitTest.deals.length).toBe(4)
+    // Manually check id's in (deals) db.json to do a deep equality test against
+    expect(storeUnitTest.deals[0].id).toEqual(6158)
   });
 
   it("should return broadband & tv deals when filtered by that criteria", () => {
     // Arrange
+    const storeUnitTest = new Store();
+    storeUnitTest.setDeals(mockData.deals);
     // Act
     // Assert
   });
 
   it("should return a broadband & mobile deal when filterd by that criteria", () => {
     // Arrange
+    const storeUnitTest = new Store();
+    storeUnitTest.setDeals(mockData.deals);
     // Act
     // Assert
   });
 
   it("should return a sky deal when filtered by that criteria", () => {
     // Arrange
+    const storeUnitTest = new Store();
+    storeUnitTest.setDeals(mockData.deals);
     // Act
     // Assert
   });
 
   it("should return bt, broadband & tv deals when filtered by that criteria", () => {
     // Arrange
+    const storeUnitTest = new Store();
+    storeUnitTest.setDeals(mockData.deals);
     // Act
     // Assert
   });
