b80ede8db2308f706b266342212804c97065c3df
Troy-S <troysheed@gmail.com>
2020-12-21 12:43:57 +0000

    Committing Unit Test Conditions #time 5m

--- src/scripts/__tests__/Store.js
+++ src/scripts/__tests__/Store.js
@@ -1,16 +1,54 @@
 import Store from "../Store";
 import mockData from "../../../public/db.json";
 
+/**
+ * Create unit tests for the following conditions:
+ *
+ * Broadband filter: Should return 4 deals
+ * Broadband & TV filter: Should return 4 deals
+ * Broadband & Mobile filter: Should return 1 deal
+ * Sky filter: Should return 1 deal
+ * BT, Broadband & TV filter: Should return 2 deals
+ */
+
 describe("filter", () => {
   it("should return all deals when no filters applied", () => {
     // Arrange
-    const sut = new Store();
-    sut.setDeals(mockData.deals);
-
+    const storeUnitTest = new Store();
+    storeUnitTest.setDeals(mockData.deals);
     // Act
-    const result = sut.deals;
-
+    const result = storeUnitTest.deals;
     // Assert
     expect(result).toEqual(mockData.deals);
+  });
+
+  it("should return ", () => {
+    // Arrange
+    // Act
+    // Assert
+  });
+
+  it("should return ", () => {
+    // Arrange
+    // Act
+    // Assert
+  });
+
+  it("should return ", () => {
+    // Arrange
+    // Act
+    // Assert
+  });
+
+  it("should return ", () => {
+    // Arrange
+    // Act
+    // Assert
+  });
+
+  it("should return ", () => {
+    // Arrange
+    // Act
+    // Assert
   });
 });
